4a11b19ffdd69172025c81f0540ff4b7d7446af6
Quan <tonlongquanvn@gmail.com>
2026-06-08 17:18:31 +0000

    fix(psql): add dependencies & fix imports

--- backend/app/configs/settings.py
+++ backend/app/configs/settings.py
@@ -1,7 +1,7 @@
-from pydantic import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    POSTGRES_URL: str = "postgresql://postgres:weatherise@localhost:5432/weatherise"
+    POSTGRES_URL: str = "postgresql+asyncpg://postgres:weatherise@localhost:5432/weatherise"
     
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     
